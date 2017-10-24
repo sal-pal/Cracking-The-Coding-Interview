f6cd8aa65562474496384d0468358c99c227e361
srpalomino <salvatpalvat@gmail.com>
2017-10-24 16:17:11 -0400

    Create psuedo code for getNewArrSize()

--- Data-Structures/arraylist.c
+++ Data-Structures/arraylist.c
@@ -1,16 +1,40 @@
 /**
-    Test Cases:
-      1) Functions like normal array
+    getSizeOfNewArr()
+        -Return value is based on how big an array needs to be in order to use the index.
+        -Input:
+            a) oldArrSize (int)
+            b) index (int)
+        -Output
+            a) sizeOfNewArr (int)
 
-      2) Functions like arraylist
-        -index is in 2nd range
-        -Index is in 5th range
+        -Error
+            a) If oldArrSize > index, then throw error message
+
+        -Steps:
+            1) If oldArrSize > index, then print error message and return
+                -perror()
+                  -Accepts a string that is the error message
+                  -Must assign correct error code to errno
+                  -https://www.tutorialspoint.com/cprogramming/c_error_handling.htm
+            2) newArrSize = oldArrSize * 2
+            3) while newArrSize <= index:
+                newArrSize = newArrSize + oldArrSize
+            4) return newArrSize
+
 **/
-
+#include<stdio.h>
 
 
 int main () {
-  //Test cases
+
+//getSizeOfNewArr() test cases
+printf("%i\n", getSizeOfNewArr(1, 2));
+printf("%i\n", getSizeOfNewArr(100, 1002));
+getSizeOfNewArr(1, 2)
+
+
+/**
+  //insert() test cases
   int arr[20];
   insert(15, 14, arr);
   insert(25, 24, arr);
@@ -21,4 +45,5 @@ int main () {
   printf("%i\n", arr[98]);
 
   return 0;
+**/
 }

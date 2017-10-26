4f1d369e7297db62b976669b8e10138c3b8110e5
srpalomino <salvatpalvat@gmail.com>
2017-10-25 20:02:16 -0400

    Almost finished implementing enlargeArr

--- Data-Structures/arraylist.c
+++ Data-Structures/arraylist.c
@@ -1,25 +1,17 @@
 /**
-    getSizeOfNewArr()
-        -Return value is based on how big an array needs to be in order to use the index.
-        -Input:
-            a) oldArrSize (int)
-            b) index (int)
-        -Output
-            a) sizeOfNewArr (int)
+    1) if index >= array.size:
+        //Insert val after increasing array size
+        +newArrSize = getNewArrSize();
+        +create new array of size newArrSize            Create new function to increase array size
+        +Place all values of oldArr into newArr
+        +insert val into newArr
+    2) insert val into oldArr
 
-        -Error
-            a) If oldArrSize > index, then throw error message
 
-        -Steps:
-            1) If oldArrSize > index, then print error message and return
-                -perror()
-                  -Accepts a string that is the error message
-                  -Must assign correct error code to errno
-                  -https://www.tutorialspoint.com/cprogramming/c_error_handling.htm
-            2) newArrSize = oldArrSize * 2
-            3) while newArrSize <= index:
-                newArrSize = newArrSize + oldArrSize
-            4) return newArrSize
+    function enlargeArr(arr, int size): void
+        1) int newArr[size]
+        2) copy arr.values to newArr
+        3) Assign newArr to variable pointed to by arr
 
 **/
 #include<stdio.h>
@@ -27,7 +19,7 @@
 
 
 int getNewArrSize(int oldArrSize, int index) {
-  if (oldArrSize > index) {
+  if (oldArrSize >= index) {
     perror("oldArrSize cannot be greater than index for function getNewArrSize");
   }
   int newArrSize = oldArrSize * 2;
@@ -38,25 +30,31 @@ int getNewArrSize(int oldArrSize, int index) {
 }
 
 
+void enlargeArr(int arr[], int size) {
+  int newArr[size];
+  int oldArrSize = size(arr[0]) / sizeof(arr);
+  for (int i=0; i < oldArrSize; i++) {
+    newArr[i] = arr[i];
+  }
+  *arr = newArr;
+}
+
+
 
 int main () {
 
-printf("%i\n", getNewArrSize(1, 2));
-printf("%i\n", getNewArrSize(100, 1002));
-getNewArrSize(2, 1);
 
+  /**
+    //insert() test cases
+    int arr[20];
+    insert(15, 14, arr);
+    insert(25, 24, arr);
+    insert(99, 98, arr);
+
+    printf("%i\n", arr[14]);
+    printf("%i\n", arr[24]);
+    printf("%i\n", arr[98]);
+  **/
 
 return 0;
-
-/**
-  //insert() test cases
-  int arr[20];
-  insert(15, 14, arr);
-  insert(25, 24, arr);
-  insert(99, 98, arr);
-
-  printf("%i\n", arr[14]);
-  printf("%i\n", arr[24]);
-  printf("%i\n", arr[98]);
-**/
 }

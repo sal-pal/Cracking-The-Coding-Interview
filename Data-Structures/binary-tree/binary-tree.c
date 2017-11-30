1f9fddf04d4928cfe36fdbd5efb9756d58da2e34
srpalomino <salvatpalvat@gmail.com>
2017-11-30 16:46:08 -0500

    Rename node to treeNode and check if queue is empty in binary-tree.c

--- Data-Structures/binary-tree/binary-tree.c
+++ Data-Structures/binary-tree/binary-tree.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-#include "../queues/queue.h"
-
-struct node {
-    int value;
-    struct node *left;
-    struct node *right;
-};
+#include "../queues/bin-tree-queue.h"
 
 
-void inorder_traversal(struct node* root) {
+
+void inorder_traversal(struct treeNode* root) {
   if (root != NULL) {
     inorder_traversal(root->left);
     printf("%i\n", root->value);
@@ -17,7 +12,7 @@ void inorder_traversal(struct node* root) {
 }
 
 
-void preorder_traversal(struct node* root) {
+void preorder_traversal(struct treeNode* root) {
   if (root != NULL) {
     printf("%i\n", root->value);
     preorder_traversal(root->left);
@@ -26,7 +21,7 @@ void preorder_traversal(struct node* root) {
 }
 
 
-void postorder_traversal(struct node* root) {
+void postorder_traversal(struct treeNode* root) {
   if (root != NULL) {
     postorder_traversal(root->left);
     postorder_traversal(root->right);
@@ -34,56 +29,64 @@ void postorder_traversal(struct node* root) {
   }
 }
 
-void breadth_first_traverse(struct node* root) {
+void traverse_breadth_first(struct treeNode* root) {
+  create();
   printf("%i\n", root->value);
+
   if (root->left != NULL) {
-    //enqueue root.left
+    enq(root->left);
   }
   if (root->right != NULL) {
-    //enqueue root.right
+    enq(root->right);
+  }
+
+  if (!queueIsEmpty()) {
+    //Deque and get front value
+    //print the value of treeNode
+    //enque the treeNode's children
   }
 }
 
 
 int main()
 {
-    //Instantiate all nodes
-    struct node root;
+    //Instantiate all treeNodes
+    struct treeNode root;
     root.value = 0;
     root.left = NULL;
     root.right = NULL;
 
-    struct node internal1;
+    struct treeNode internal1;
     internal1.value = 1;
     internal1.left = NULL;
     internal1.right = NULL;
 
-    struct node internal2;
+    struct treeNode internal2;
     internal2.value = 2;
     internal2.left = NULL;
     internal2.right = NULL;
 
-    struct node leaf1;
+    struct treeNode leaf1;
     leaf1.value = 3;
     leaf1.left = NULL;
     leaf1.right = NULL;
 
-    struct node leaf2;
+    struct treeNode leaf2;
     leaf2.value = 4;
     leaf2.left = NULL;
     leaf2.right = NULL;
 
-    struct node leaf3;
+    struct treeNode leaf3;
     leaf3.value = 5;
     leaf3.left = NULL;
     leaf3.right = NULL;
 
-    struct node leaf4;
+    struct treeNode leaf4;
     leaf4.value = 6;
     leaf4.left = NULL;
     leaf4.right = NULL;
 
-    //Make each node point to its subtrees
+    //Make each treeNode point to its subtrees
     root.left = &internal1;
     root.right = &internal2;
     internal1.left = &leaf1;
@@ -91,7 +94,7 @@ int main()
     internal2.left = &leaf3;
     internal2.right = &leaf4;
 
-    struct node *rootPntr = &root;
+    struct treeNode *rootPntr = &root;
     traverse_breadth_first(rootPntr);
 
 

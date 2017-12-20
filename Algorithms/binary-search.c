44ed408fe71421f2ff18c47c8c63367558f24536
srpalomino <salvatpalvat@gmail.com>
2017-12-19 19:29:59 -0500

    Begin debugging binary_search()

--- Algorithms/binary-search.c
+++ Algorithms/binary-search.c
@@ -1,13 +1,38 @@
-#include<stdlib.h>
+#include<stdio.h>
 
 
-int binary_search(int arr[]) {
-
+int binary_search(int arr[], int x, int n) {
+  int mid;
+  int first = 0;
+  int last = n - 1;
+  while (first != last) {
+    mid = (last + first) / 2;
+    if (arr[mid] == x) {
+      return mid + 1;
+    }
+    else {
+      if (x > arr[mid]) {
+        first = mid + 1;
+      }
+      else {
+        last = mid - 1;
+      }
+    }
+  }
 }
 
 
 int main() {
   int arr[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
   //Should return 11
-  int result = binary_search(arr);
+  int result = binary_search(arr, 2444, 25);
+  printf("%i\n", result);
+  return 0;
 }
+
+
+
+/**
+    Prob: answer is 24 when x ~E of arr
+      -
+**/

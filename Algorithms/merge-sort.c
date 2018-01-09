e7b057243f510e2d4edde393fd95ade138becf1f
srpalomino <salvatpalvat@gmail.com>
2018-01-08 20:30:36 -0500

    Finish debugging merge-sort.c

--- Algorithms/merge-sort.c
+++ Algorithms/merge-sort.c
@@ -3,7 +3,7 @@
 void merge(int left[], int right[], int leftSize, int rightSize, int A[]) {
     int l=0, r=0, a=0;
     while (l < leftSize && r < rightSize) {
-        if (left[l] > right[r]) {
+        if (left[l] < right[r]) {
           A[a] = left[l];
           l++; a++;
         }
@@ -18,13 +18,13 @@ void merge(int left[], int right[], int leftSize, int rightSize, int A[]) {
     }
     while (r < rightSize) {
       A[a] = right[r];
-      l++; a++;
+      r++; a++;
     }
 }
 
 int main() {
-  int left[4] = {8,5,4,6};
-  int right[4] = {12,9,10,1};
+  int left[4] = {1,3,5,15};
+  int right[4] = {8,9,11,20};
   int a[8];
   merge(left, right, 4, 4, a);
   for (int i=0; i < 8; i++) {

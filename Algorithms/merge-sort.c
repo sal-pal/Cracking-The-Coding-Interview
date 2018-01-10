12eaf928703a8b84402c0bb734c50dca79392f41
srpalomino <salvatpalvat@gmail.com>
2018-01-09 22:50:28 -0500

    Finish implementing  slice()

--- Algorithms/merge-sort.c
+++ Algorithms/merge-sort.c
@@ -23,8 +23,40 @@ void merge(int left[], int right[], int leftSize, int rightSize, int A[]) {
 }
 
 
+void slice(int startIndex, int endIndex, int arrToSlice[], int outputArr[]) {
+    int j = 0;
+    for (int i=startIndex; i <= endIndex; i++) {
+      outputArr[j] = arrToSlice[i];
+      j++;
+    }
+}
+
+
 void mergeSort(int arr[], int arrSize) {
   /**
+      1) If arrSize < 2:
+        a) return from the function
+      2) Create left array
+      3) Find size of left array
+      4) Create right array
+      5) Find size of right array
+      6) Pass left array to mergeSort()
+      7) Pass right array to mergeSort()
+      8) merge left and right arrays
+
+
+      2) Create left array
+        -Split the single array in half
+        -Also know the size of the new array.
+        Steps:
+          1) Find the size of the new array
+          2) Select all elements from 0-(size-1)
+
+
+      4) Create right array
+        1) Find size of the right array
+        2) Select all elements from leftSize - arrSize
+
 
   **/
 }

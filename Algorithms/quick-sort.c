c1a298385113349d40099a157c8b7a01bf30c044
srpalomino <salvatpalvat@gmail.com>
2018-01-15 19:02:19 -0500

    Finish implementing partition() with no bugs

--- Algorithms/quick-sort.c
+++ Algorithms/quick-sort.c
@@ -1,31 +1,34 @@
 #include<stdio.h>
 
 void swap(int arr[], int i, int j) {
-  if (i == j) {
-    printf("\nError: i and j cannot be equal\n\n");
-    return;
-  }
 
   int iElem = arr[i];
   int jElem = arr[j];
 
   arr[i] = jElem;
   arr[j] = iElem;
+}
 
+
+int partition(int arr[], int start, int end) {
+  int pivot = arr[end];
+  int pIndex = start;
+  for (int i=start; i < end; i++) {
+    int elem = arr[i];
+    if (elem <= pivot) {
+      swap(arr, i, pIndex);
+      pIndex++;
+    }
+  }
+  swap(arr, pIndex, end);
+  return pIndex;
 }
 
 
 int main() {
-  int arr1[] = {0,1,2,3,4};
-  int arr2[] = {0,1};
-  int arr3[] = {0};
-
-  swap(arr1, 1, 3);
-  swap(arr2, 0, 1);
-  swap(arr3, 0, 0);
-
-  printf("arr1:\n 1st index: %i\n", arr1[1]);
-  printf(" 3rd index: %i\n\n", arr1[3]);
-  printf("arr2:\n 0th index: %i\n", arr2[0]);
-  printf(" 1st index: %i\n\n", arr2[1]);
+  int arr[] = {4,3,2,1,0};
+  partition(arr, 0, 3);
+  for (int i=0; i < 5; i++) {
+    printf("%i\n", arr[i]);
+  }
 }
